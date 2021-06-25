09eee48b0bf990a60197f287c335236e1db16597
AlexD <alexandredesoutter.pro@gmail.com>
2021-06-25 15:36:49 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+
+// On récupère la librairie de sequelize
+const {Datatypes, Sequelize} = require('sequelize');
+
+const sequelize = new Sequelize(
+    'mariadb://quizz_admin:root1234@localhost/Quizz'
+)
+
+// un Objet JS qui représente une table de la BDD
+const Category = sequelize.define('Category', {
+    name: {
+        type: Datatypes.STRING, //VARCHAR 255
+        allowNull: false      // NOT NULL
+    }
+})
+
+
+console.log('Checking Database connection...');
+
+sequelize.authenticate()
+.then(() => {
+    console.log("Database connection OK!");
+    sequelize.sync({force:true})
+
+    app.listen(PORT, () => {
+        console.log(`Web server running at localhost:${PORT}`);
+    });
+})
+
+.catch((err) => {
+    console.log(err);
+})
