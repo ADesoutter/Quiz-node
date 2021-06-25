4d374e6e888de209f38aa57cc357437cb5a51923
AlexD <alexandredesoutter.pro@gmail.com>
2021-06-25 17:14:42 +0200

    ajout de routers

--- models/Category.js
+++ models/Category.js
@@ -1,7 +1,12 @@
-//   CREATE TABLE category ( 
-// `id` INT UNSIGNED NOT NULL AUTO_INCREMENT , 
-// `name` VARCHAR(190) NOT NULL , 
-// `creation_date` DATETIME NULL , 
-// `modification_date` DATETIME NULL , 
-// PRIMARY KEY (`id`)) ENGINE = InnoDB;
+// DataTypes est à importer dans chaque fichier de modèle
+const {DataTypes} = require('sequelize');
 
+module.exports = (sequelize) => sequelize.define('Category', {
+    name: {
+        type: DataTypes.STRING(190),
+        allowNull: false
+    } 
+ }, {
+     tableName: 'category',
+     underscored: true
+ })
